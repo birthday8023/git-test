2ac6280c795d6650aa77e874453c8f891cf77716
sunxiaoyang <2385945957@qq.com>
2024-02-02 15:42:10 +0800

    master commit1

--- src/main/java/com/sunxy/git/GitTest.java
+++ src/main/java/com/sunxy/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello3");
         System.out.println("hello5");
         System.out.println("hello7");
+        System.out.println("master test!");
     }
 }
